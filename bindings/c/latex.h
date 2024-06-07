5725a4ad5a403fe2d22ef1f7a8701285a7efe0e3
Jakub Panek <me@panekj.dev>
2024-06-07 23:48:03 +0200

    fix: tree-sitter generate

---
+++ bindings/c/latex.h
@@ -0,0 +1,16 @@
+#ifndef TREE_SITTER_LATEX_H_
+#define TREE_SITTER_LATEX_H_
+
+#include <tree_sitter/parser.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern TSLanguage *tree_sitter_latex();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TREE_SITTER_LATEX_H_
